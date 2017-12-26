32a0f7ed927cf709c1823525b234edd90a68f714
LightProspect <17701080544@163.com>
2017-12-26 14:51:59 +0800

    增加了bean类

---
+++ app/src/main/java/com/v/gyyx/base/zhangqifan.java
@@ -0,0 +1,15 @@
+package com.v.gyyx.base;
+
+/**
+ * 作者-->蜕变~成蝶
+ * 创建于-->2017/12/26
+ * 作用-->
+ */
+
+public class zhangqifan {
+    String zhao;
+
+    public zhangqifan(String zhao) {
+        this.zhao = zhao;
+    }
+}
